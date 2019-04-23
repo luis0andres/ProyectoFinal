de58784f4b14ba3a4551302f14aab80490288072
isra <isactorrez@gmail.com>
2019-04-23 17:19:15 -0400

    Permisos-Toolbar-SharedPreferences

--- app/src/main/java/com/univalle/proyectofinal/LogInActivity.java
+++ app/src/main/java/com/univalle/proyectofinal/LogInActivity.java
@@ -1,7 +1,14 @@
 package com.univalle.proyectofinal;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        final TextView user = findViewById(R.id.editText);
+        final TextView pass = findViewById(R.id.editText2);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+                String usuario = user.getText().toString();
+                String password = pass.getText().toString();
+
+                SharedPreferences.Editor editor = preferences.edit();
+
+                editor.putString("usuario",usuario);
+                editor.putString("contraseña",password);
+
+                Toast.makeText(LogInActivity.this,"Guardado con exito",Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/univalle/proyectofinal/MainActivity.java
+++ app/src/main/java/com/univalle/proyectofinal/MainActivity.java
@@ -2,6 +2,10 @@ package com.univalle.proyectofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar tool = findViewById(R.id.toolbar2);
+
+        setSupportActionBar(tool);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public boolean OnCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return  true;
+    }
+
+    public boolean OnOptionsItemSelected(MenuItem menuItem)
+    {
+        switch (menuItem.getItemId())
+        {
+            case R.id.opc1:
+                Toast.makeText(this,"Ha presionado la opcion 1",Toast.LENGTH_SHORT);
+                break;
+            case R.id.opc2:
+                Toast.makeText(this,"Ha presionado la opcion 2",Toast.LENGTH_SHORT);
+                break;
+            case R.id.opc3:
+                Toast.makeText(this,"Ha presionado la opcion 3",Toast.LENGTH_SHORT);
+                break;
+            case R.id.opc4:
+                Toast.makeText(this,"Ha presionado la opcion 4",Toast.LENGTH_SHORT);
+                break;
+            case android.R.id.home:
+                Toast.makeText(this,"Ha presionado la flechita de atras",Toast.LENGTH_SHORT);
+                break;
+
+        }
+        return true;
+    }
+
+
 }
